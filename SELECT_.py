14de02c85a4b2700a52f6e30d1213790bf4d97aa
Maxim Pavlenko 77 <max.pavlenko@gmail.com>
2023-03-11 18:17:06 +0300

    Task3_11_03_2023

--- SELECT_.py
+++ SELECT_.py
@@ -1,1 +1,22 @@
+SELECT album_name, release_year FROM albums
+WHERE release_year = 2018;
+
+SELECT record_name, record_duration FROM records
+ORDER BY record_duration DESC
+LIMIT 1;
+
+SELECT record_name FROM records
+WHERE record_duration >= '00:03:30';
+
+SELECT collection_name FROM collections
+WHERE release_year BETWEEN 2018 AND 2020;
+
+SELECT singer_name FROM singers
+WHERE singer_name NOT LIKE '% %';
+
+SELECT record_name FROM records
+WHERE record_name LIKE ANY (ARRAY ['мой %', '% мой %', '% мой',
+'Мой %', '% Мой %', '% Мой',
+'my %', '% my %', '% my',
+'My %', '% My %', '% My']);
 
